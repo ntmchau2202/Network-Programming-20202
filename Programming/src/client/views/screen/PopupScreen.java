ed0195877d8c10fef01bba9de47e9bd6b5140d27
phucnguyen111 <phucnguyen0111@gmail.com>
2021-06-25 17:00:56 +0700

    minor popup

--- Programming/src/client/views/screen/PopupScreen.java
+++ Programming/src/client/views/screen/PopupScreen.java
@@ -40,28 +40,28 @@ public class PopupScreen extends BaseScreenHandler{
     }
 
     public static void success(String message) throws IOException{
-        popup(message, Misc.getImageByName(Configs.IMAGE_PATH) + "/" + "checked.png", false).show(true);
+        popup(message, Configs.IMAGE_PATH + "/" + "checked.png", true).show(true);
     }
 
     public static void gameOver(String message) throws IOException{
-        popup(message, Misc.getImageByName(Configs.IMAGE_PATH) + "/" + "game-over.png", false).show(false);
+        popup(message, Configs.IMAGE_PATH + "/" + "game-over.png", false).show(false);
     }
 
     public static void win(String message) throws IOException{
-        popup(message, Misc.getImageByName(Configs.IMAGE_PATH) + "/" + "winner.png", false).show(false);
+        popup(message, Configs.IMAGE_PATH + "/" + "winner.png", false).show(false);
     }
 
     public static void lose(String message) throws IOException{
-        popup(message, Misc.getImageByName(Configs.IMAGE_PATH) + "/" + "lose.png", false).show(false);
+        popup(message, Configs.IMAGE_PATH + "/" + "lose.png", false).show(false);
     }
 
 
     public static void error(String message) throws IOException{
-        popup(message, Misc.getImageByName(Configs.IMAGE_PATH) + "/" + "warn.png", false).show(false);
+        popup(message, Configs.IMAGE_PATH + "/" + "warn.png", false).show(false);
     }
 
     public static PopupScreen loading(String message) throws IOException{
-        return popup(message, Misc.getImageByName(Configs.IMAGE_PATH) + "/" + "loading.gif", true);
+        return popup(message, Configs.IMAGE_PATH + "/" + "loading.gif", true);
     }
 
     public void setImage(String path) {
